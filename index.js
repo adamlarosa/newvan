db2a62d71028b28d48aad34f507b2dbd1d422c85
Adam La Rosa <adam.larosa@gmail.com>
2020-06-09 18:42:15 -0700

    states sorted

--- index.js
+++ index.js
@@ -7,15 +7,63 @@ let data;
 
 let states = {};
 
+fetchData = () => {
+    console.log("FETCH!")
+    fetch(`${covidURL}${covidPATH}${covidSLUG}`)
+        .then(resp => resp.json())
+        .then(json => {
+            data = json;
+            console.log("fetch complete!")
+            sortFetch(data)
+        })
+}
+
 provinceInStates = (entry) => {
     return Object.keys(states).includes(entry.Province)
 }
 
-console.log("FETCH!!")
-fetch(`${covidURL}${covidPATH}${covidSLUG}`)
-    .then(resp => resp.json())
-    .then(json => {
-        data = json;
-        console.log("complete!")
-    })
+cityInProvince = (entry) => {
+    return Object.keys(states[entry["Province"]]).includes(entry.City)
+}
 
+addProvinceToStates = (entry) => {
+    const { Province, City } = entry;
+    states[Province] = {};
+    states[Province][City] = [];
+    states[Province][City].push(entry);
+}
+
+addEntryToStates = (entry) => {
+    const { Province, City } = entry;
+    states[Province][City].push(entry)
+}
+
+addCityToProvince = (entry) => {
+    const { Province, City } = entry;
+    states[Province][City] = [];
+    states[Province][City].push(entry);
+}
+
+sortFetch = (data) => {
+    for (const index in data) {
+    let entry = data[index];
+        if (provinceInStates(entry)) {
+            if (cityInProvince(entry)) {
+                addEntryToStates(entry);
+            } else {
+                addCityToProvince(entry)
+            }
+        } else {
+            addProvinceToStates(entry);
+        }
+    }
+    console.log("Sort complete!")
+}
+
+
+loadData = async () => {
+    await fetchData();
+    await sortFetch(data)
+}
+
+loadData();
